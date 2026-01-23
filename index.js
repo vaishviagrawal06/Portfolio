a450ba8c742cf12d54cb1d5446a0b70718fe72dc
vaishviagrawal06 <vaishviagrawal06@gmail.com>
2026-01-23 11:52:50 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+
+// let theme = document.querySelector('button')
+// let body = document.querySelector('body')
+
+// btn.addEventListener("click",function(){
+//     if( body.style.backgroundColor==='[#0b0b0b]/80'){
+//         body.style.backgroundColor='white';
+//     }
+//     else{
+
+//       body.style.backgroundColor='[#0b0b0b]/80';  
+//     }
+   
+// });
